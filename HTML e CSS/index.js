c851bced2039dc5a8024526cd1bd97f2f2ce76b6
Guilherme <gm589072@gmail.com>
2023-10-28 16:23:53 -0300

    Finalização do Back-end da tela de login

--- HTML e CSS/index.js
+++ HTML e CSS/index.js
@@ -1,8 +1,40 @@
-const express = require("express");
+const express = require('express');
+const session = require('express-session');
+const bodyparser = require('body-parser');
+
+const port = 3000;
+var path = require('path');
 const app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+var login = 'gerente';
+var senha = '123';
+
+app.use(session({secret:'2h1vbjwbqndj2b1i4k1n4'}));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.post('/', function(req,res){
+
+    console.log(req.body.password);
+    if(req.body.password == senha && req.body.login == login ){
+        req.session.login = login;
+        res.redirect('/');
+    }else{
+        res.render('index');
+    }
 });
 
-app.listen(8080);
+app.get('/', function(req,res){
+    if(req.session.login){
+        res.render('p');
+    }else{
+        res.render('index');
+    }
+});
+
+app.listen(port, ()=>{
+    console.log('Site está rodando no localhost:3000');
+});
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Controle_Estoque
+
+## Instalação
+
+Para utilizar esse repositório precisamos clonar ele no seu computador com 
+
+```
+git clone https://github.com/GuilhLopes/Controle_Estoque.git
+```
+
+após isso devemos verificar se temos o *node.js* instalado no computador, utilizamos o comando 
+
+```
+node -v
+```
+
+caso precise instalar o node utilize esse link: https://nodejs.org/en
+
+### instalando dependencias
+
+para instalar as depencias utilizamos o comando:
+
+```
+npm install
+```
+
+após a instalação devemos ter um diretório *node_modules* no nossos arquivos.
+
+Para rodarmos o site no local host usaremos o comando:
+
+```
+nodemon index.js
+```
+
